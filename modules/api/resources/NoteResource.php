fe85d91509a1ed52b84cd6650ee27e7e1bf83111
Malkevich Maria <malkevihma@gmail.com>
2021-06-23 15:55:49 +0700

    Notebook v1

---
+++ modules/api/resources/NoteResource.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\modules\api\resources;
+
+
+use app\models\Note;
+
+/**
+ * Class NoteResource
+ *
+ * @author Maria Malkevich <malkevihma@gmail.com>
+ * @package app\modules\api\resources
+ */
+class NoteResource extends Note
+{
+
+}
